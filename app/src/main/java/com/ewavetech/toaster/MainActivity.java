42977dd0d87edbef99c1b549e40a3eadf0d5a588
Adil Sher <adilsher7@gmail.com>
2022-11-03 02:03:16 +0500

    add toast package

--- app/src/main/java/com/ewavetech/toaster/MainActivity.java
+++ app/src/main/java/com/ewavetech/toaster/MainActivity.java
@@ -3,6 +3,7 @@ package com.ewavetech.toaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.ewavetech.toasterlib.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToasterMessage.show(this, "Show Message from library");
     }
 }
