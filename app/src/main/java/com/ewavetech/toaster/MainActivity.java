202cbfc7d8765a4d31272a86f116697bde1d53b8
Adil Sher <adilsher7@gmail.com>
2022-11-05 01:58:15 +0500

    add toast package

--- app/src/main/java/com/ewavetech/toaster/MainActivity.java
+++ app/src/main/java/com/ewavetech/toaster/MainActivity.java
@@ -3,7 +3,10 @@ package com.ewavetech.toaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import com.ewavetech.toasterlib.ToasterMessage;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToasterMessage.show(this, "Show Message from library");
+        TextView textView = findViewById(R.id.text_view);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClassName("com.ewavetech.toaster", "com.ewavetech.toasterlib.ToastLibActivity");
+                startActivity(intent);
+            }
+        });
+        //ToasterMessage.show(this, "Calling from Github Library");
     }
 }
